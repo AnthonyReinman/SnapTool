feb51fa15aba836908b5bd4837d73b77f39e97cf
Anthony Reinman <ajreinman@student.fullsail.edu>
2023-11-11 18:42:38 -0800

    Initial commit - Take Picture working and Basic UI created - Clean

---
+++ app/src/main/java/com/example/snaptool/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.example.snaptool
+
+import android.Manifest
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.snaptool.ui.theme.SnapToolTheme
+import androidx.activity.compose.rememberLauncherForActivityResult
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SnapToolTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CameraFeature()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CameraFeature() {
+    var imageBitmap by remember { mutableStateOf<Bitmap?>(null) }
+    val context = LocalContext.current
+
+    // Camera launcher
+    val cameraLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.TakePicturePreview(),
+        onResult = { bitmap ->
+            imageBitmap = bitmap
+        }
+    )
+
+    // Permission launcher
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted: Boolean ->
+            if (isGranted) {
+                cameraLauncher.launch(null)
+            } else {
+                Toast.makeText(context, "Camera permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { permissionLauncher.launch(Manifest.permission.CAMERA) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Take Picture")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp)) // Add space between the button and the image
+
+        imageBitmap?.let { bitmap ->
+            // Display the image with some padding and a fixed height
+            Image(
+                bitmap = bitmap.asImageBitmap(),
+                contentDescription = "Captured Image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .padding(16.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SnapToolTheme {
+        CameraFeature()
+    }
+}
