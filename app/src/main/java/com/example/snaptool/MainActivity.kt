7b297597ad555ee362e798100293bc6ddc0546c0
Anthony Reinman <ajreinman@student.fullsail.edu>
2023-11-19 15:31:19 -0800

    RekognitionAPI - not calling

--- app/src/main/java/com/example/snaptool/MainActivity.kt
+++ app/src/main/java/com/example/snaptool/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.snaptool
 import android.Manifest
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
@@ -20,28 +23,124 @@ import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.rekognition.AmazonRekognitionClient
+import com.amazonaws.services.rekognition.model.DetectLabelsRequest
 import com.example.snaptool.ui.theme.SnapToolTheme
-import androidx.activity.compose.rememberLauncherForActivityResult
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.nio.ByteBuffer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SnapToolTheme {
+                var imageBitmap by remember { mutableStateOf<Bitmap?>(null) }
+                var toolName by remember { mutableStateOf("") }
 
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CameraFeature()
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        CameraFeature(imageBitmap) { capturedBitmap ->
+                            imageBitmap = capturedBitmap
+                            analyzeImageWithRekognition(capturedBitmap) { name ->
+                                toolName = name
+                            }
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // This Text composable displays the tool name
+                        if (toolName.isNotEmpty()) {
+                            Text(
+                                text = "Tool Identified: $toolName",
+                                style = MaterialTheme.typography.bodyMedium 
+                            )
+                        }
+                        imageBitmap?.let { bitmap ->
+                            Image(bitmap = bitmap.asImageBitmap(),
+                                contentDescription = "Captured Image",
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(300.dp)
+                                    .padding(16.dp))
+
+                        }
+                    }
                 }
+            }
+        }
+    }
+
+private fun analyzeImageWithRekognition(bitmap: Bitmap, onResult: (String) -> Unit) {
+    Log.d("Rekognition", "Starting image analysis")
+    val base64Image = convertToBase64(bitmap)
+
+    // Setup AWS credentials
+    val awsCredentials = BasicAWSCredentials("AKIASIQPUJRXEZFILVVG", "c8T78iNoyrbH4EHi7lD2cIzqw+N6dXI7SwhBCXsi")
+    val rekognitionClient = AmazonRekognitionClient(awsCredentials)
+
+    // Prepare the request
+    val request = DetectLabelsRequest()
+        .withImage(com.amazonaws.services.rekognition.model.Image()
+            .withBytes(ByteBuffer.wrap(Base64.decode(base64Image, Base64.DEFAULT))))
+        .withMaxLabels(10)
+        .withMinConfidence(75F)
+
+    CoroutineScope(Dispatchers.IO).launch {
+        try {
+            // Send the request and handle the response
+            val response = rekognitionClient.detectLabels(request)
+            Log.d("Rekognition", "Response received: ${response.labels}")
+            val labelName = response.labels.maxByOrNull { it.confidence }?.name ?: "Unknown"
+
+            // Switch to the Main dispatcher to update the UI
+            withContext(Dispatchers.Main) {
+                onResult(labelName)
+                //LOGS
+                Log.d("UIUpdate", "Tool name updated: $labelName")
+            }
+        } catch (e: Exception) {
+            Log.e("Rekognition", "Error: ${e.localizedMessage}")
+            withContext(Dispatchers.Main) {
+                onResult("Error: ${e.localizedMessage}")
+                //LOGS
+                Log.e("UIUpdate", "Error updating tool name: ${e.localizedMessage}")
             }
         }
     }
 }
 
+    // Send the request and handle the response
+   /* val response = rekognitionClient.detectLabels(request)
+    response.labels.forEach { label ->
+        // Process and use label data
+        println("${label.name}: ${label.confidence}")
+    }
+}*/
+
+private fun convertToBase64(bitmap: Bitmap): String {
+    ByteArrayOutputStream().apply {
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, this)
+        return Base64.encodeToString(toByteArray(), Base64.DEFAULT)
+    }
+}
+}
+
 @Composable
-fun CameraFeature() {
+fun CameraFeature(imageBitmap: Bitmap?, onImageCaptured: (Bitmap) -> Unit) {
     var imageBitmap by remember { mutableStateOf<Bitmap?>(null) }
     val context = LocalContext.current
 
@@ -49,7 +148,12 @@ fun CameraFeature() {
     val cameraLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.TakePicturePreview(),
         onResult = { bitmap ->
-            imageBitmap = bitmap
+            if (bitmap != null) {
+                Log.d("CameraFeature", "Image captured successfully")
+                imageBitmap = bitmap
+            } else {
+                Log.d("CameraFeature", "Failed to capture image")
+            }
         }
     )
 
@@ -81,17 +185,7 @@ fun CameraFeature() {
 
         Spacer(modifier = Modifier.height(16.dp)) // Add space between the button and the image
 
-        imageBitmap?.let { bitmap ->
-            // Display the image with some padding and a fixed height
-            Image(
-                bitmap = bitmap.asImageBitmap(),
-                contentDescription = "Captured Image",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(300.dp)
-                    .padding(16.dp)
-            )
-        }
+
     }
 }
 
@@ -99,6 +193,6 @@ fun CameraFeature() {
 @Composable
 fun DefaultPreview() {
     SnapToolTheme {
-        CameraFeature()
+        CameraFeature(imageBitmap = null, onImageCaptured = {})
     }
 }
---
+++ app/src/main/java/com/example/snaptool/RekognitionActivity.kt
@@ -0,0 +1,28 @@
+package com.example.snaptool
+
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+/*import com.amazonaws.services.rekognition.AmazonRekognitionClient
+import com.amazonaws.services.rekognition.model.DetectLabelsRequest
+import com.amazonaws.services.rekognition.model.Image
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.rekognition.model.S3Object*/
+import android.graphics.Bitmap
+import android.util.Base64
+import java.io.ByteArrayOutputStream
+import java.nio.ByteBuffer
+
+class RekognitionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rekognition)
+
+        // Your code for Rekognition integration goes here
+    }
+
+    // Additional methods as needed
+}
